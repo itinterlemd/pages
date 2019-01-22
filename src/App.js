4750145dd81cf3fb3ab19b632c1bbf9728f809ab
itinterlemd <itinterlemd@gmail.com>
2019-01-21 23:04:05 -0500

    configs for github.io

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-import { Switch, Route ,Redirect, HashRouter } from 'react-router-dom'
+import { Switch, Route ,Redirect, BrowserRouter} from 'react-router-dom'
 import Main from "./components/main";
 import Terminos from "./components/terminos_interlemd";
+import './index.css';
+
 
 
 class App extends Component {
   render() {
+    console.log('process.env.PUBLIC_URL-->'+ process.env.PUBLIC_URL)
     return (
-      <HashRouter>
+      <BrowserRouter basename="/pages" >
     <Switch >
+
       <Route path="/terminos" component={Terminos}/>
       <Route path="/home" component={Main}/>      
       <Route  path="/principal"> 
@@ -19,7 +23,7 @@ class App extends Component {
       </Route>     
       <Route component={() => (<div>404 Not found </div>)} />
       </Switch>
-    </HashRouter>
+    </BrowserRouter>
     );
   }
 }
