2d759b796faa7122d059bb662889cd60de3d914f
itinterlemd <itinterlemd@gmail.com>
2019-01-21 22:04:03 -0500

    Updates

---
+++ precache-manifest.2374056111e91ee74adbc542b4ecad0c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c4aa2813f00e751ea725f882aba4035",
+    "url": "C:/Program Files/Git/static/media/itli@1.5c4aa281.jpeg"
+  },
+  {
+    "revision": "48b10de323d975a60303",
+    "url": "C:/Program Files/Git/static/css/main.bcbb71d9.chunk.css"
+  },
+  {
+    "revision": "3e4b74eee10be0266045",
+    "url": "C:/Program Files/Git/static/js/1.3e4b74ee.chunk.js"
+  },
+  {
+    "revision": "d7775c80a7c0029d9536",
+    "url": "C:/Program Files/Git/static/js/runtime~main.d7775c80.js"
+  },
+  {
+    "revision": "37cc9514680e3ecde616e590cff841d0",
+    "url": "C:/Program Files/Git/static/media/bannerITLI.37cc9514.jpg"
+  },
+  {
+    "revision": "086934e2563e490d4309ddad6be8bed8",
+    "url": "C:/Program Files/Git/static/media/banner-pc.086934e2.jpg"
+  },
+  {
+    "revision": "48b10de323d975a60303",
+    "url": "C:/Program Files/Git/static/js/main.48b10de3.chunk.js"
+  },
+  {
+    "revision": "b0aa499a3d7ab09c2800890096191881",
+    "url": "C:/Program Files/Git/static/media/volkan-olmez-73767-unsplash.b0aa499a.jpg"
+  },
+  {
+    "revision": "1f14dc570a5d148b2c7f75c05aa83b98",
+    "url": "C:/Program Files/Git/static/media/taduuda-72915-unsplash.1f14dc57.jpg"
+  },
+  {
+    "revision": "a8ec8bd2109090bbb37e8cddd2d340ba",
+    "url": "C:/Program Files/Git/static/media/rawpixel-196509-unsplash.a8ec8bd2.jpg"
+  },
+  {
+    "revision": "a3d039b9371f599da77377a91defcb46",
+    "url": "C:/Program Files/Git/static/media/rawpixel-284723-unsplash.a3d039b9.jpg"
+  },
+  {
+    "revision": "e99dcadcdf10eaceb5d0cd581a5fd236",
+    "url": "C:/Program Files/Git/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f2f1a28591b17b63c45569801697024e.js"
+  "C:/Program Files/Git/precache-manifest.2374056111e91ee74adbc542b4ecad0c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("C:/Program Files/Git/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
