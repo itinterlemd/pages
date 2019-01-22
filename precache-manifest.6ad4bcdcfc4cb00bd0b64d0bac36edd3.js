98d81a8b0f0b2000b57302a52be8daadcc693638
itinterlemd <itinterlemd@gmail.com>
2019-01-21 21:36:51 -0500

    Updates

---
+++ precache-manifest.6ad4bcdcfc4cb00bd0b64d0bac36edd3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "086934e2563e490d4309ddad6be8bed8",
+    "url": "/pages/static/media/banner-pc.086934e2.jpg"
+  },
+  {
+    "revision": "1fcd64f9f83375012a8a",
+    "url": "/pages/static/css/main.2aa47f8c.chunk.css"
+  },
+  {
+    "revision": "3e4b74eee10be0266045",
+    "url": "/pages/static/js/1.3e4b74ee.chunk.js"
+  },
+  {
+    "revision": "fd506ed7c1c5f1396356",
+    "url": "/pages/static/js/runtime~main.fd506ed7.js"
+  },
+  {
+    "revision": "37cc9514680e3ecde616e590cff841d0",
+    "url": "/pages/static/media/bannerITLI.37cc9514.jpg"
+  },
+  {
+    "revision": "5c4aa2813f00e751ea725f882aba4035",
+    "url": "/pages/static/media/itli@1.5c4aa281.jpeg"
+  },
+  {
+    "revision": "1fcd64f9f83375012a8a",
+    "url": "/pages/static/js/main.1fcd64f9.chunk.js"
+  },
+  {
+    "revision": "b0aa499a3d7ab09c2800890096191881",
+    "url": "/pages/static/media/volkan-olmez-73767-unsplash.b0aa499a.jpg"
+  },
+  {
+    "revision": "1f14dc570a5d148b2c7f75c05aa83b98",
+    "url": "/pages/static/media/taduuda-72915-unsplash.1f14dc57.jpg"
+  },
+  {
+    "revision": "a3d039b9371f599da77377a91defcb46",
+    "url": "/pages/static/media/rawpixel-284723-unsplash.a3d039b9.jpg"
+  },
+  {
+    "revision": "a8ec8bd2109090bbb37e8cddd2d340ba",
+    "url": "/pages/static/media/rawpixel-196509-unsplash.a8ec8bd2.jpg"
+  },
+  {
+    "revision": "033d1e00b9f5aaf59ebaf9a3a8a4f1b9",
+    "url": "/pages/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c238fcd50799e2a00e714b89fcae8976.js"
+  "/pages/precache-manifest.6ad4bcdcfc4cb00bd0b64d0bac36edd3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/pages/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
